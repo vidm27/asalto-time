9bc36e45fd8214c5d3623428b512107baeee7162
vidm27 <davidmoran464@gmail.com>
2024-04-14 09:01:12 -0500

    refact: Add const to route

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -4,6 +4,6 @@ import 'package:go_router/go_router.dart';
 import 'package:asalto_time/timer/pages/pages.dart';
 
 final appRouter = GoRouter(routes: [
-  GoRoute(path: '/',name: HomeScreen.name, builder: (context, state) => HomeScreen()),
+  GoRoute(path: '/',name: HomeScreen.name, builder: (context, state) => const HomeScreen()),
   GoRoute(path: '/config-round',name: ConfigRoundPage.name, builder: (context, state) => const ConfigRoundPage()),
 ]);
