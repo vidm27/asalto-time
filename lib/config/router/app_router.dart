6a316fed998602aca55961799beb1ec766b6d97c
vidm27 <davidmoran464@gmail.com>
2024-04-14 08:45:33 -0500

    build(core): importar archivo y configurar ruta

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,8 +1,9 @@
 
 
-import 'package:asalto_time/timer/pages/home_page.dart';
 import 'package:go_router/go_router.dart';
+import 'package:asalto_time/timer/pages/pages.dart';
 
 final appRouter = GoRouter(routes: [
-  GoRoute(path: '/',name: HomeScreen.name, builder: (context, state) => HomeScreen())
+  GoRoute(path: '/',name: HomeScreen.name, builder: (context, state) => HomeScreen()),
+  GoRoute(path: '/config-round',name: ConfigRoundPage.name, builder: (context, state) => const ConfigRoundPage()),
 ]);
